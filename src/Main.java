a6a3fd3c471b1e6b3a2503efe2e15edc284f179a
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2026-02-04 09:02:26 -0300

    O(long n)
    Increase array size to 100 and populate it with ascending values (0..99) instead of random numbers. Replace the linear search with a binary search using start/end/mid and a counter (total) for iterations. Also update user prompt and result messages, and print array elements during initialization.

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,43 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] vector = new int[8];
+        int[] vector = new int[100];
 
-        for (int i=0; i < vector.length; i++){
-            vector[i] = (int) (Math.random() * 8);
+        for (int i=0; i < vector.length; i++ ){
+            vector[i] = i;
             System.out.println(vector[i]);
         }
 
         boolean find = false;
+        int total = 0;
+        int start = 0;
+        int end = vector.length - 1;
 
-        System.out.print("qual numero vc busca?: ");
+        System.out.print("Qual numero vc quer buscar?: ");
         int number = sc.nextInt();
 
-        for (int i : vector){
-           if (i == number){
-               find = true;
-               break;
-           }
+        while (start <= end) {
+            int mid = (int) (start + end) / 2;
+            total++;
+            if(vector[mid] == number){
+                find = true;
+                break;
+            }
+            else if(vector[mid] < number){
+                start = mid + 1;
+
+            }
+            else{
+                end = mid - 1;
+            }
+
         }
         if (find){
-            System.out.println("Achou o numero!");
+            System.out.println("Achou e contou: " + total + " Vezes.");
+
         }
         else {
-            System.out.println("Não Achou o numero!");
+            System.out.println("Não achou");
         }
 
 
