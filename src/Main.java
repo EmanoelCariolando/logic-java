42c08d58ae7d7feae68a5dc6a877e32385545438
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2026-02-08 21:52:59 -0300

    Add recursive sum and shrink vector to 5
    Reduce the vector length from 10 to 5 and replace the selection-sort / ordering logic with a recursive adding(int sum, int position, int[] vect) method that computes the total. The program still prints the random vector values and now prints total: followed by the computed sum. Note: Scanner remains declared but unused.

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] vector = new int[10];
+        int[] vector = new int[5];
 
         System.out.println("disordering");
         for (int i=0; i < vector.length; i++ ){
             vector[i] = (int) (Math.random() * vector.length);
             System.out.println(vector[i]);
         }
+        System.out.print("total: ");
+        System.out.print(adding(0,0,vector));
+    }
 
-        int smaller, assis;
-        for (int i = 0; i < vector.length; i++){
-            smaller = i;
-             for (int j = i+1; j < vector.length; j++){
-                if (vector[j] < vector[smaller]){
-                    smaller = j;
-                }
-              }
+     private static int adding(int sum, int position, int[] vect){
+     if (position < vect.length){
+        sum = sum + vect[position];
+        return adding(sum, position+1, vect);
+     }
+     else {
+      return sum;
+     }
 
-            assis = vector[smaller];
-            vector[smaller] = vector[i];
-            vector[i] = assis;
-
-        }
-
-        System.out.println("ordered");
-        for (int i=0; i < vector.length; i++ ){
-            System.out.println(vector[i]);
-        }
     }
 }
 
