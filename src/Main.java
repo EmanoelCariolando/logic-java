376554ec8304ac29699c017f96443b96cb3d56e9
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2026-02-12 11:27:52 -0300

    Implement heap sort and expand vector
    Change vector size from 5 to 10 and replace the recursive summing routine with a heap sort implementation. Adds a heapSort(vect, n, i) helper (sift-down), a build-heap loop and extraction loop, and prints intermediate states ("disordering", "almost ordering", "ordering"). Removes the old recursive adding(...) and total output; preserves random initialization of the array.

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,54 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] vector = new int[5];
+        int[] vector = new int[10];
 
         System.out.println("disordering");
         for (int i=0; i < vector.length; i++ ){
             vector[i] = (int) (Math.random() * vector.length);
             System.out.println(vector[i]);
         }
-        System.out.print("total: ");
-        System.out.print(adding(0,0,vector));
+
+        int n = vector.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--){
+            heapSort(vector,n,i);
+        }
+        System.out.println("almost ordering");
+        for (int i = 0; i < vector.length; i++){
+         System.out.println(vector[i] + "");
+        }
+        for (int j = n-1; j > 0; j--){
+            int aux = vector[0];
+            vector[0] = vector[j];
+            vector[j] = aux;
+
+            heapSort(vector,n,j);
+        }
+
+        System.out.println("ordering");
+        for (int i = 0; i < vector.length; i++){
+            System.out.println(vector[i] + "");
+        }
     }
-
-     private static int adding(int sum, int position, int[] vect){
-     if (position < vect.length){
-        sum = sum + vect[position];
-        return adding(sum, position+1, vect);
+    private static void heapSort(int[] vect,int n,int i){
+     int source = i;
+     int left = 2*i + 1;
+     int right = 2*i + 2;
+     
+     if (left < n && vect[left] > vect[source]){
+         source = left;
      }
-     else {
-      return sum;
+     if (right < n && vect[right] > vect[source]){
+         source = right;
      }
+     if(source != i){
+         int aux = vect[i];
+         vect[i] = vect[source];
+         vect[source] = aux;
 
+         heapSort(vect,n,source);
+     }
     }
 }
 
